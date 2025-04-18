7d83430def107d1064781b8952a82355feae4346
Sri Tikkireddy <sri.tikkireddy+github@databricks.com>
2025-04-18 09:57:16 -0400

    fixed warning for cpu

--- lod/event_listeners/cpu.py
+++ lod/event_listeners/cpu.py
@@ -5,7 +5,7 @@ from locust.env import Environment
 
 @events.cpu_warning.add_listener
 def handle_cpu_warning(*args, environment: Environment, cpu_usage: float, **kwargs):
-    logging.info(f"cpu warning hit at {cpu_usage}%")
+    logging.error(f"CPU OVER UTILIZATION: {cpu_usage}%")
     logging.error("STOPPING SWARM EARLY...")
     logging.error("Please use larger instance type, distributed or reduce the number of users.")
     environment.runner.quit()
