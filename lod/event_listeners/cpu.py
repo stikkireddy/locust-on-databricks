1e73bd5a6f968f39f78fb1942ca77058254d2382
Sri Tikkireddy <sri.tikkireddy+github@databricks.com>
2025-04-18 09:53:22 -0400

    quit

--- lod/event_listeners/cpu.py
+++ lod/event_listeners/cpu.py
@@ -8,4 +8,4 @@ def handle_cpu_warning(*args, environment: Environment, cpu_usage: float, **kwar
     logging.info(f"cpu warning hit at {cpu_usage}%")
     logging.error("STOPPING SWARM EARLY...")
     logging.error("Please use larger instance type, distributed or reduce the number of users.")
-    environment.runner.stop()
+    environment.runner.quit()
